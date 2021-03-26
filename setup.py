ed10615c9134731f5ef615b1bbab192b49f28bf2
virbickt <63246400+virbickt@users.noreply.github.com>
2021-03-26 16:51:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,16 @@ from distutils.core import setup
 setup(
     name='scraperuodas',
     version='1.0',
-    packages=['scraperuodas']
+    packages=['scraperuodas'],
+    url='https://github.com/virbickt/scraperuodas',
+    license='MIT License',
+    author='Teofilius Virbickas',
+    author_email='tvirbickas@gmail.com',
+    description='Automatic data collection tool specifically design to collect data from aruodas.lt',
+    install_requires=[
+        'requests',
+        'bs4',
+        'pandas',
+        'fake_useragent'
+    ]
 )
