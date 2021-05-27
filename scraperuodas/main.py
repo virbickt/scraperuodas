f92a5f58cd84d2f01c64feaaa11a599f2dd66d59
virbickt <63246400+virbickt@users.noreply.github.com>
2021-05-27 12:48:24 +0300

    updated class documentation

--- scraperuodas/main.py
+++ scraperuodas/main.py
@@ -9,14 +9,37 @@ from random import randint
 
 
 class Scraperuodas:
-    """Automated data collection tool (web-scraper) specifically tailored to scrape data on aruodas.lt."""
+    """
+    Automated data collection tool (web-scraper) specifically tailored to scrape data on aruodas.lt.
+    
+    Parameters:
+    
+        num_samples (int): The number of samples of data to be scraped. One sample amounts to a single listing on
+        a search results page. The minimum of 27 samples can be provided.
+        
+        city (str): The name of the city that the listings are based in.
+        Default value of the parameter is "Vilnius".
+        Cities the listings for which can currently be found on aruodas.lt:
+        Vilnius, Kaunas, Klaipėda, Šiauliai, Panevėžys, Alytus, Palanga.
+        
+        max_price(int): Maximum price to limit the search results by.
+        The parameter corresponds to the query parameter "MaxPrice".
+        
+    Returns:
+    
+        pd.DataFrame: Returns a dataframe with the following categories as columns:
+        name of the district, name of the street, price (euros per month), price per square mater (abbreviated as price_per_sm),
+        area (area of the room/apartment listed for rent in square meters), floor the room/apartment listed is on
+        and the total number of floors on the building the apartment is on.
+        The number of samples corresponds to the number of rows in the returned table.
+        
+    """
     def __init__(self) -> None:
         self.num_samples = 0
         self.max_price = 0
 
     def generate_urls(self, num_samples: int, city: str, max_price: int) -> list:
-        # There's 27 listings on a single page
-        # Make sure to allow for scraping less than 27 pages
+        # Note that there's 27 listings on a single page
         city_path = city
         page_path = 'puslapis/'
         query_params = {"FOrder": "AddDate", "FPriceMax": max_price}
